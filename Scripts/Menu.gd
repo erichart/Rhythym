d578f299453153ba50bf6ce675e5ce34bd9d3647
erichart <hart.eric.r@gmail.com>
2024-09-04 15:47:47 -0400

    inital commit

---
+++ Scripts/Menu.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _on_Start_Button_button_down():
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), 
+								linear_to_db($HSlider.value))
+	if get_tree().change_scene_to_file("res://Scenes/Game.tscn") != OK:
+		print ("Error changing scene to Game")
+
+
+func _on_HSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"),
+								linear_to_db(value))
+
+
+func _on_TextureButton_pressed():
+	$TextureButton/AudioStreamPlayer.play()
